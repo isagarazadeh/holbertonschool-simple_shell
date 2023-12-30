4917f469e3c5b2f9df60c016d256e54b1874e3e4
isagarazadeh <garazadeisa625@gmail.com>
2023-12-30 01:51:47 -0800

    correction

--- str_cmp.c
+++ str_cmp.c
@@ -35,7 +35,7 @@ int _strncmp(const char *str1, const char *str2, size_t n)
 
 	do {
 		if (*str1 != *str2++)
-			return (*(unsigned char *)str1 - *(unsigned char *)--str2);
+			return (*(unsigned char *)str1 - *(unsigned char *) --str2);
 		if (*str1++ == 0)
 			break;
 	} while (--n != 0);
